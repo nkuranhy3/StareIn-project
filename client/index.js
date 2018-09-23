a55cf8019edf681d54378b4f7181df4f072b8499
Alfred Boateng <programmingwithalfred@gmail.com>
2018-09-23 18:51:03 +0000

    index.html

--- client/index.js
+++ client/index.js
@@ -4,44 +4,41 @@ var cfUtil = require('aws-cloudfront-sign');
 // your CloudFront key pair.
 var cfPk =
 '-----BEGIN RSA PRIVATE KEY-----\n' +
-'MIIEowIBAAKCAQEAg5esWfHu+f/tBMTqYrR2F12OF6JGqo6Y7ckxAcjLmkhktzm4\n' +
-'wniiCI4XoBB4v8+LBd8PrBu8PBy7HqWHywC7epBNPY7z1CouG4H80mdWKzSL5PAz\n' +
-'wiDaF2c5wWcf5Czz3m36zs3+//ftOpPAgCXsTBjSGhbl5W1RFGGdI0ccyxj7zEJO\n' +
-'VWoAIepW6dLdC1wxD6Qykr3yPXGSUaj3w12lpIdDgknuT6+LvlZsvGC4gKNecrZS\n' +
-'USRwRgNMjf9S69Rv1rWLh8+KjktAn8s3BfSxDFpcey0w+qYLXsjREdypLwRconUI\n' +
-'XVP8ZNEVZkoklckSQDtRAr4hzIGk7VJ0xT2iSQIDAQABAoIBAAsrhcLAyZNJCRp9\n' +
-'iJPGU7vaAiS5bpwtrIeKq3FGVRoMiRgQe0Fusfpd7V116HoHqPpfm9n1JL5Jhgys\n' +
-'6mDt9/wIYbYW+1WIGw/Usdw+T08tj65oQUx6M2kHJxzmnwtkUXMRDNgfHkUmC2oM\n' +
-'fquiDHPNK5BxXyRhah97xbXn5GbM4d+3xFbGZB2TSF29SOctoXEydHV7+zdzvtNQ\n' +
-'X5Ibees3x8R8Cf2nyrhgMJ4Rv6HktGMBE3I2hpePzrrlyjmy9fgrR6UlnCnJylYX\n' +
-'YdqweEMnjc6En5T7bZ0P2niD2vnFtSzW8F5UzL31LSazM0/43Ib0zu+kQmyKQSqW\n' +
-'TcgztS0CgYEAxFu0D8pMo7Md68x2K2znTSgY3OqcU0wJ5rD270rhILZuOej7T7NS\n' +
-'83kWD4wdI/zxOfsizW2ZCYi5/sEzvjq05YZMflzf1yeK1O/Qac8rRp6bObkvjY9g\n' +
-'G4C2KfAFnLcQK6aUkzaP6oPY00HxBZQPhasXnlyvttK+qOOx5MmUNLsCgYEAq4/0\n' +
-'6Jos1Ak3vGHOspwtWWWvbcXuUv4nXrp04Ul0GMFKL41CW1iWMUYVeal1naR9zoSr\n' +
-'kOLmzX6LgsfY66Tfrp3ZBcj/YPbWUdSfjL0xAVcRaxhktv63KSNDUNSyw4Ao4n9Y\n' +
-'jk+fWAWapu2GLCj4bui9RBeHThXY/qr96qlzVssCgYAVOMAGMNwFmTczo95lpRh+\n' +
-'i0XconKQwhgT2a5mgFZCfdT950M6AuyFKeILfh4sJItBs/W39A9lK2TGtsTL23Zv\n' +
-'lAsyYQFENJ7Zkq2Yse5AbKXkIDKWfxdpAYkukSVaIaLaxN8r5eq58tP9SRQugSkI\n' +
-'Tkx4LhZDkRVQFYPFcU/WPwKBgATFBcwJrUFf2DrWhUaV7PTVTOFCZXPNOQ77JBC3\n' +
-'WBDwHr3fsX6UoehQyHwPaAOv9QduzM5EYKJJ6FsEb1VtygIyxTvR8LejZSX1QVJK\n' +
-'WDhvUT6m+PXVcwYjwtVWdHnW8EwLukyKjiWGUlxdsiM8KFv9AHYKKWrUVKoP2utR\n' +
-'NrGtAoGBAIVa0xlM1780j9zx9HkdbRdSf5B6hYG8wYnJafgGWehqUC33nDktjGwk\n' +
-'Kxq9REeR94h5DofS2Y+erylUPKVcXkYewVLcYH5rg5XMIZbD1VY+/qmRSqqVXSeI\n' +
-'TXQPLP37fDCfVvs12KygdIz0NHsKsTgltwBkAuy301pQtMSd4wzb\n' +
+'MIIEogIBAAKCAQEAgBmGbFU3bxnZpqMQ2LwmFP4lq7RauurKCF623Snm1XGNQuF9\n' +
+'XqDeK3TH3ZfYC6P4iQ+C+Ynw15UP/MGbULO2UCmLfkA30FyI/u46jdhdD7hvMqEj\n' +
+'UOEBxVJGFhqrZyerd9A7dRqYS6DTbaz3Vb+aGNcBLuqPP9/TydkkqoFqQnft43W7\n' +
+'mWPp7Cx+TDkY/untwF3TWJdiAeke3FBAB2mni+BlmrNQs3vfufhW2XMV8sSOY+cN\n' +
+'7chQmruV1stS+KCGiFfkiel824KI/1yVUe7+ofDGJF7v1G6WD4XV2sBAz01EIWSK\n' +
+'vo1txA1lSoRcFHmnNOB4d8dKncilxEjstq6J5QIDAQABAoIBAC/m26CJIUiXdw9c\n' +
+'LQGPIgJ5oyaZM9Bv2grVODZt49bvNm7LGYqAZNmuOmzIhLk12+Lzbo7hV+Vp908G\n' +
+'znfj3zT6Ucmu9nFDuKibMq+hZIg/30VnS0AcU+gwwuqjIHNbn0AXixRYVJj7U/TQ\n' +
+'WFEla/9b7yJXLigpj+4MGCz2ZgDifr3YrCLucqozHagFf7apJ0xguqG7dYqOIrnp\n' +
+'67CzIc/U76qkT/hVMwlC6Kl3VJg2nIIsQQv73bi0MLr4VzMl7ca+h7iCoGuZOBOn\n' +
+'vbRyFnfQW0Dr07THHmgQvCZiIE4PkHk2hgTA0yVFFkYOZ6z6xyqoT8Qs+eUVGXCP\n' +
+'Au5h6WECgYEA/fjlmzHgMnyfsCugmd/Qbh4tyDVBET6jKKG/JI/K43DjTTLWthcx\n' +
+'Rlse1B6L3fPiaLyNiLQWwdLnH/KHuVEY1om6eTO7WQLAEtTUOngsXms33ZdHtzIj\n' +
+'r6UW9yqiDG6wNHH3Ql8oJCMaKs8z/mrcPJut0JORLmqd68NeOyxeIi0CgYEAgR9a\n' +
+'TG2L06zJZ2Zk6sFee/4nZ5HgMHavxt25/JJtLG4Rew/lb1N10QcSk3v4I7bl41uB\n' +
+'QhlHfyYd1yb0a2iTckW/5OZA1oRHliP/Vx95NS0ti3tO1hsuPKVTrMTEpEB2lul3\n' +
+'BQuZehOE9HCW2QlDnwBeM2SDA0kagknIh63XsZkCgYBgEkIQxfowPvJNOwOikYaP\n' +
+'0TyySmrVsiMYIK9kjjxKcw6Yyk1sTjOk9FkWYP3SwHqfEs0L4hSn6u3F9/34bp+N\n' +
+'fmtkUTW0WK3G0jtYV5XiegCEvZnelmxe9g1M7ESmfUyMWjwVUFen69tfLEhXymaL\n' +
+'SryidN/rdgtM/vdrXOoy9QKBgAks4izGKAZ9o74uP4OTBBTJhaFNc2HePTVjciDp\n' +
+'gsqCc8mL4qDbjGazGvXR/FsFVyalzPaddcweu0kaziZdm36Z1JPI4o1fMUijtVax\n' +
+'voXJvfjVtWGgAbgj05NayZohX/14B9YG8fwDwRHhokZ/6wc0bn02ajzkh/a0KYTC\n' +
+'rK4ZAoGAGqYbrwHYFFgAOhOaPdER9jK+MXWl1pUhdFTUbNETgF0Nay06GifY+1DA\n' +
+'oTu2hg3k7z5464WANk/ixn5nlyRD/i8Ab4ENA56sFly9qOyEdWlXKNrocMd4wjJr\n' +
+'ZVF3wvEieF2E1PTySKYNb0ZUm70nfzMj6sRFw9ow58LdpPVXIew=\n' +
 '-----END RSA PRIVATE KEY-----'
-
 // Sample key pair ID. This would need to be replaced by the Access Key ID from
 // your CloudFront key pair.
-var cfKeypairId = 'APKAITDBJOSZILKFRADQ';
-var cfURL = 'http://d10n1fj1bacjeh.cloudfront.net/path/to/s3/leg.mp4';
-
-
-var today = new Date();
+var cfKeypairId = 'APKAJM2FEVTI7BNPCY4A';
+var cfURL = 'http://xxxxxxx.cloudfront.net/path/to/s3/object';
 
 var signedUrl = cfUtil.getSignedUrl(cfURL, {
   keypairId: cfKeypairId,
-  expireTime: Date.now()+ 600000000000,
+  expireTime: Date.now() + 60000,
   privateKeyString: cfPk
 });
+
 console.log(signedUrl);
